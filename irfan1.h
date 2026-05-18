f82c24c83075927e0f5943b0b3068d6167c7a5b1
irfanardiansah320-oss <irfanardiansah320@gmail.com>
2026-05-18 16:50:32 +0700

    fix

--- irfan1.h
+++ irfan1.h
@@ -9,12 +9,6 @@ void createNewFile(void);
 // Keluar dari program
 void exitEditor(void);
 
-// Menjalankan editor teks untuk file tertentu
-// head: pointer ke head linked list (dokumen)
-// filename: nama file untuk menyimpan
-// isNewFile: 1 jika file baru, 0 jika membuka file yang sudah ada (opsional)
-void runEditor(Node **head, const char *filename, int isNewFile);
-
 // Menangani editing teks (backspace, enter, karakter biasa)
 // ch: karakter yang ditekan
 // cursor: cursor editor dengan linked list dan posisi kursor
